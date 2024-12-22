ace77ffb5fd35622b6716a7ea1a1626c785a960d
Roma5640 <138190068+XxktotosxX@users.noreply.github.com>
2024-12-22 23:19:14 +0200

    map_spritesDemo

---
+++ scenes/Anim_Blum.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Blum_anim/Animatedblum.play("default")
+	$Blum_anim/Animatedblum2.play("default")
+	$Blum_anim/Animatedblum3.play("default")
+	$Blum_anim/Animatedblum4.play("default")
+	$Blum_anim/Animatedblum5.play("default") 
+	$Blum_anim/Animatedblum6.play("default")
+	$Blum_anim/Animatedblum7.play("default")
+	$Blum_anim/Animatedblum8.play("default")
+	$Blum_anim/Animatedblum9.play("default")
+	$Blum_anim/Animatedblum10.play("default")
+	$Blum_anim/Animatedblum11.play("default")
+	$Blum_anim/Animatedblum12.play("default")
+	$Blum_anim/Animatedblum13.play("default")
+	$Blum_anim/Animatedblum14.play("default")
