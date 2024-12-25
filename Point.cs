ea9fe3987274b79708ea12ac83186ca956161d1d
Domi <73461001+Domiurk@users.noreply.github.com>
2024-12-25 11:19:53 +0200

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -4,13 +4,15 @@ namespace StepToStep;
 
 public partial class Point : TouchScreenButton
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    public override void _Ready()
+    {
+        base._Ready();
+        this.Pressed += PressedToPoint;
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    private void PressedToPoint()
+    {
+        GD.Print(Transform.X);    
+        GD.Print(Transform.Y);    
+    }
 }
