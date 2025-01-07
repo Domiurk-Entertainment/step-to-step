3fa34bad067b218e989ed12ce1b7d08c9f353c85
Roma5640 <138190068+XxktotosxX@users.noreply.github.com>
2025-01-07 18:19:21 +0200

    testmap

--- scripts/points/Point.cs
+++ scripts/points/Point.cs
@@ -5,27 +5,27 @@ namespace StepToStep;
 
 public partial class Point : Node2D
 {
-    public event Action<Vector2> ClickedToPoint;
-    [Export]
-    private Button button;
-    [Export]
-    private Vector2 offset = new Vector2(30, -25);
+	public event Action<Vector2> ClickedToPoint;
+	[Export]
+	private Button button;
+	[Export]
+	private Vector2 offset = new Vector2(30, -25);
 
-    private void OnPressed()
-    {
-        ClickedToPoint?.Invoke(GlobalPosition + offset);
-        button.ReleaseFocus();
-    }
+	private void OnPressed()
+	{
+		ClickedToPoint?.Invoke(GlobalPosition + offset);
+		button.ReleaseFocus();
+	}
 
-    public override void _EnterTree()
-    {
-        base._EnterTree();
-        button.Pressed += OnPressed;
-    }
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		button.Pressed += OnPressed;
+	}
 
-    public override void _ExitTree()
-    {
-        base._ExitTree();
-        button.Pressed -= OnPressed;
-    }
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		button.Pressed -= OnPressed;
+	}
 }
