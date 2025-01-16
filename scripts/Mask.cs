1d70980187dbf65120670841693d35b824dc7af1
Domi <73461001+Domiurk@users.noreply.github.com>
2025-01-16 22:53:38 +0200

    Paused Menu (not paused function)

--- scripts/Mask.cs
+++ scripts/Mask.cs
@@ -8,8 +8,17 @@ public partial class Mask : Sprite2D
     [Export] private Node2D player;
     [Export] private Vector2 offset;
 
+    public override void _Ready()
+    {
+        base._Ready();
+        if(player is null)
+            GD.Print($"Player is not found. {GetTree().CurrentScene.Name} -> {GetTree()}");
+    }
+
     public override void _Process(double delta)
     {
+        if(player is null)
+            return;
         GlobalPosition = player.GlobalPosition + offset;
     }
 }
