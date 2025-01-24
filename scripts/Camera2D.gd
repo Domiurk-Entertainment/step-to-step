7a612a9b61d7dd4a55af897f899a7b6120dcfcdb
Domi <73461001+Domiurk@users.noreply.github.com>
2025-01-24 18:14:21 +0200

    Fix few bugs

---
+++ scripts/Camera2D.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+
+var shake_duration = 0.2
+var shake_amplitude = 6.5
+var shake_timer = 0.1
+var original_position = Vector2(76, -65)
+
+func start_shake():
+	original_position = position
+	shake_timer = shake_duration
+
+func _process(delta):
+	if shake_timer > 0:
+		shake_timer -= delta
+		if shake_timer <= 0:
+			position = original_position
+		else:
+			var offset = Vector2(randf_range(-shake_amplitude, shake_amplitude), randf_range(-shake_amplitude, shake_amplitude))
+			position = original_position + offset
