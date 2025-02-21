01a0f2aa7ce80e377d27f68c809c3e4cf6f314f1
Roma5640 <138190068+XxktotosxX@users.noreply.github.com>
2025-02-21 16:39:27 +0200

    Add DemoSoudMainMenu

--- scripts/Sight.cs
+++ scripts/Sight.cs
@@ -5,85 +5,85 @@ namespace StepToStep.Battle;
 
 public partial class Sight : Node2D
 {
-    private const int UP = 1;
-    private const int DOWN = -1;
+	private const int UP = 1;
+	private const int DOWN = -1;
 
-    private const int STOP = 0;
-    private const int PLAY = 1;
+	private const int STOP = 0;
+	private const int PLAY = 1;
 
-    private const string PROPERTY_NAME = "rotation_degrees";
-    public event Action<Vector2> CalculatedDirection;
+	private const string PROPERTY_NAME = "rotation_degrees";
+	public event Action<Vector2> CalculatedDirection;
 
-    [ExportCategory("Settings")]
-    [Export] private float minValue = -70;
-    [Export] private float maxValue = 70;
-    [Export] private float duration = 1;
-    [Export] private Tween.TransitionType transition = Tween.TransitionType.Linear;
+	[ExportCategory("Settings")]
+	[Export] private float minValue = -70;
+	[Export] private float maxValue = 70;
+	[Export] private float duration = 1;
+	[Export] private Tween.TransitionType transition = Tween.TransitionType.Linear;
 
-    [ExportCategory("Nodes")]
-    [Export] private Node2D startPoint;
-    [Export] private Node2D endPoint;
+	[ExportCategory("Nodes")]
+	[Export] private Node2D startPoint;
+	[Export] private Node2D endPoint;
 
-    private int state;
-    private int direction;
+	private int state;
+	private int direction;
 
-    private Tween tween;
+	private Tween tween;
 
-    public override void _Ready()
-    {
-        RotationDegrees = minValue;
-        state = STOP;
-        direction = UP;
-    }
+	public override void _Ready()
+	{
+		RotationDegrees = minValue;
+		state = STOP;
+		direction = UP;
+	}
 
-    private Vector2 Calculate() => (endPoint.GlobalPosition - startPoint.GlobalPosition).Normalized();
+	private Vector2 Calculate() => (endPoint.GlobalPosition - startPoint.GlobalPosition).Normalized();
 
-    public bool TryShoot()
-    {
-        if(state == PLAY){
-            EndRotation();
-            return true;
-        }
+	public bool TryShoot()
+	{
+		if(state == PLAY){
+			EndRotation();
+			return true;
+		}
 
-        tween = ContinueTween();
-        state = PLAY;
-        return false;
-    }
+		tween = ContinueTween();
+		state = PLAY;
+		return false;
+	}
 
-    private void EndRotation()
-    {
-        state = STOP;
-        tween.Pause();
-        CalculatedDirection?.Invoke(Calculate());
-    }
+	private void EndRotation()
+	{
+		state = STOP;
+		tween.Pause();
+		CalculatedDirection?.Invoke(Calculate());
+	}
 
-    private Tween CreateNewTween()
-    {
-        Tween result = CreateTween();
+	private Tween CreateNewTween()
+	{
+		Tween result = CreateTween();
 
-        if(RotationDegrees <= minValue){
-            SetProperty(result, minValue, maxValue);
-            direction = UP;
-        }
-        else{
-            SetProperty(result, maxValue, minValue);
-            direction = DOWN;
-        }
+		if(RotationDegrees <= minValue){
+			SetProperty(result, minValue, maxValue);
+			direction = UP;
+		}
+		else{
+			SetProperty(result, maxValue, minValue);
+			direction = DOWN;
+		}
 
-        result.Finished += () => tween = CreateNewTween();
-        return result;
-    }
+		result.Finished += () => tween = CreateNewTween();
+		return result;
+	}
 
-    private Tween ContinueTween()
-    {
-        Tween result = CreateTween();
-        SetProperty(result, RotationDegrees, direction == DOWN ? minValue : maxValue);
-        result.Finished += () => tween = CreateNewTween();
-        return result;
-    }
+	private Tween ContinueTween()
+	{
+		Tween result = CreateTween();
+		SetProperty(result, RotationDegrees, direction == DOWN ? minValue : maxValue);
+		result.Finished += () => tween = CreateNewTween();
+		return result;
+	}
 
-    private void SetProperty(Tween tween, float from, float to) => tween
-                                                                   .TweenProperty(this, PROPERTY_NAME, to, duration)
-                                                                   .From(from)
-                                                                   .SetTrans(transition);
+	private void SetProperty(Tween tween, float from, float to) => tween
+																   .TweenProperty(this, PROPERTY_NAME, to, duration)
+																   .From(from)
+																   .SetTrans(transition);
 }
--- scripts/roma/main_menu.gd
+++ scripts/roma/main_menu.gd
@@ -6,8 +6,44 @@ func _process(delta):
 
 
 func _on_quit_pressed():
+	$AudioStreamPlayer2D3.play()
 	get_tree().quit()
+	
 
 
 func _on_play_pressed():
+	$AudioStreamPlayer2D3.play()
 	SceneTransition.change_scene("res://scenes/map_sprite.tscn")
+	
+
+
+func _on_play_mouse_entered():
+	$AudioStreamPlayer2D2.play()
+	$play/PointLight2D.show()
+
+func _on_quit_mouse_entered():
+	$AudioStreamPlayer2D2.play()
+	$quit/PointLight2D2.show()
+	
+func _on_return_mouse_entered():
+	$AudioStreamPlayer2D2.play()
+
+
+func _on_play_mouse_exited():
+	$play/PointLight2D.hide()
+
+
+func _on_quit_mouse_exited():
+	$quit/PointLight2D2.hide()
+
+
+func _on_settings_mouse_entered():
+	$AudioStreamPlayer2D2.play()
+
+
+func _on_settings_pressed():
+	$AudioStreamPlayer2D3.play()
+
+
+func _on_return_pressed():
+	$AudioStreamPlayer2D3.play()
