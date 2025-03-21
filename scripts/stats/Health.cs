f5c371efa137cc3ecf4d44e11148d15ca673958c
Roma5640 <138190068+XxktotosxX@users.noreply.github.com>
2025-03-21 21:03:25 +0200

    add new test forest map

--- scripts/stats/Health.cs
+++ scripts/stats/Health.cs
@@ -6,53 +6,53 @@ namespace StepToStep;
 [GlobalClass]
 public partial class Health : ProgressBar
 {
-    public event Action<float> ChangedValue;
-    public event Action DecreasedValue;
+	public event Action<float> ChangedValue;
+	public event Action DecreasedValue;
 
-    [ExportCategory("Float Number")]
-    [Export] private Font _font;
-    [Export] private int _fontSize = 21;
-    [Export] private Color _color = new Color(1f, 1f, 1f);
+	[ExportCategory("Float Number")]
+	[Export] private Font _font;
+	[Export] private int _fontSize = 21;
+	[Export] private Color _color = new Color(1f, 1f, 1f);
 
-    [ExportCategory("Tween Float Number")]
-    [Export] private float _duration = 1;
-    [Export] private float _length = 50;
-    [Export] private Tween.TransitionType _transitionType = Tween.TransitionType.Elastic;
+	[ExportCategory("Tween Float Number")]
+	[Export] private float _duration = 1;
+	[Export] private float _length = 50;
+	[Export] private Tween.TransitionType _transitionType = Tween.TransitionType.Elastic;
 
-    public void Subtract(Node sender, float value)
-    {
-        if(value < 0)
-            throw new Exception($"{sender} sending failure value ({value})");
-        Value -= (value);
-        ChangedValue?.Invoke((float)Value);
-        DecreasedValue?.Invoke();
-        CreateFloatNumbers(value);
-    }
+	public void Subtract(Node sender, float value)
+	{
+		if(value < 0)
+			throw new Exception($"{sender} sending failure value ({value})");
+		Value -= (value);
+		ChangedValue?.Invoke((float)Value);
+		DecreasedValue?.Invoke();
+		CreateFloatNumbers(value);
+	}
 
-    public void Add(object sender, float value)
-    {
-        if(value < 0)
-            throw new Exception($"{sender} sending failure value ({value})");
-        Value += value;
-        ChangedValue?.Invoke((float)Value);
-        CreateFloatNumbers(value, "+#.##");
-    }
+	public void Add(object sender, float value)
+	{
+		if(value < 0)
+			throw new Exception($"{sender} sending failure value ({value})");
+		Value += value;
+		ChangedValue?.Invoke((float)Value);
+		CreateFloatNumbers(value, "+#.##");
+	}
 
-    private void CreateFloatNumbers(float value, string format = "-#.##")
-    {
-        var label = new Label();
-        Vector2 finalVal = Vector2.Up * _length;
-        label.LabelSettings = new LabelSettings();
-        label.LabelSettings.FontSize = _fontSize;
-        label.LabelSettings.Font = _font;
-        label.SelfModulate = _color;
-        label.Text = value.ToString(format);
-        label.Position = new Vector2((float)GD.RandRange(0, Size.X), (float)GD.RandRange(0, Size.Y));
-        AddChild(label);
+	private void CreateFloatNumbers(float value, string format = "-#.##")
+	{
+		var label = new Label();
+		Vector2 finalVal = Vector2.Up * _length;
+		label.LabelSettings = new LabelSettings();
+		label.LabelSettings.FontSize = _fontSize;
+		label.LabelSettings.Font = _font;
+		label.SelfModulate = _color;
+		label.Text = value.ToString(format);
+		label.Position = new Vector2((float)GD.RandRange(0, Size.X), (float)GD.RandRange(0, Size.Y));
+		AddChild(label);
 
-        Tween tween = CreateTween().SetParallel().SetTrans(_transitionType);
-        tween.TweenProperty(label, "position", label.Position + finalVal, _duration);
-        tween.TweenProperty(label, "self_modulate", new Color(1, 1, 1, 0), _duration);
-        tween.Finished += label.QueueFree;
-    }
+		Tween tween = CreateTween().SetParallel().SetTrans(_transitionType);
+		tween.TweenProperty(label, "position", label.Position + finalVal, _duration);
+		tween.TweenProperty(label, "self_modulate", new Color(1, 1, 1, 0), _duration);
+		tween.Finished += label.QueueFree;
+	}
 }
--- scripts/utilities/SaveSystem.cs
+++ scripts/utilities/SaveSystem.cs
@@ -8,130 +8,130 @@ namespace StepToStep.Systems;
 
 public partial class SaveSystem : Node
 {
-    public static SaveSystem Instance;
+	public static SaveSystem Instance;
 
-    [Export] private string pathConfig = "user://";
-    [Export] private string fileName = "config.cfg";
-    [Export] private bool canSave = true;
+	[Export] private string pathConfig = "user://";
+	[Export] private string fileName = "config.cfg";
+	[Export] private bool canSave = true;
 
-    private Dictionary<string, Dictionary<string, Variant>> _dataToSave = new();
+	private Dictionary<string, Dictionary<string, Variant>> _dataToSave = new();
 
-    public override void _Ready()
-    {
-        if(Instance != null && Instance != this){
-            GD.PrintErr($"{Name} is duplicate. {Instance.Name} is original.");
-            QueueFree();
-            return;
-        }
-        else{
-            Instance = this;
-        }
+	public override void _Ready()
+	{
+		if(Instance != null && Instance != this){
+			GD.PrintErr($"{Name} is duplicate. {Instance.Name} is original.");
+			QueueFree();
+			return;
+		}
+		else{
+			Instance = this;
+		}
 
-        if(_dataToSave.Count == 0)
-            foreach(TypeConfiguration typeConfiguration in Enum.GetValues<TypeConfiguration>())
-                _dataToSave.Add(typeConfiguration.ToString(), new Dictionary<string, Variant>());
+		if(_dataToSave.Count == 0)
+			foreach(TypeConfiguration typeConfiguration in Enum.GetValues<TypeConfiguration>())
+				_dataToSave.Add(typeConfiguration.ToString(), new Dictionary<string, Variant>());
 
-        if(!ExistConfig())
-            new ConfigFile().Save(GetFullPath());
-        GD.Print(ExistConfig());
-    }
+		if(!ExistConfig())
+			new ConfigFile().Save(GetFullPath());
+		GD.Print(ExistConfig());
+	}
 
-    public void RemoveDataFromKey(TypeConfiguration typeConfiguration, string key)
-    {
-        ConfigFile config = GetConfigFile();
-        ConfigFile newConfig = new ConfigFile();
+	public void RemoveDataFromKey(TypeConfiguration typeConfiguration, string key)
+	{
+		ConfigFile config = GetConfigFile();
+		ConfigFile newConfig = new ConfigFile();
 
-        foreach(string section in config.GetSections()){
-            foreach(string sectionKey in config.GetSectionKeys(section)){
-                if(section == typeConfiguration.ToString() && key == sectionKey){
-                    continue;
-                }
+		foreach(string section in config.GetSections()){
+			foreach(string sectionKey in config.GetSectionKeys(section)){
+				if(section == typeConfiguration.ToString() && key == sectionKey){
+					continue;
+				}
 
-                Variant data = config.GetValue(section, sectionKey);
-                newConfig.SetValue(section, sectionKey, data);
-            }
-        }
+				Variant data = config.GetValue(section, sectionKey);
+				newConfig.SetValue(section, sectionKey, data);
+			}
+		}
 
-        newConfig.Save(GetFullPath());
-    }
+		newConfig.Save(GetFullPath());
+	}
 
-    private bool ExistConfig()
-        => !File.Exists(GetFullPath());
+	private bool ExistConfig()
+		=> !File.Exists(GetFullPath());
 
-    public void RemoveAllData()
-    {
-        ConfigFile config = GetConfigFile();
-        config.Clear();
-        config.Save(GetFullPath());
-    }
+	public void RemoveAllData()
+	{
+		ConfigFile config = GetConfigFile();
+		config.Clear();
+		config.Save(GetFullPath());
+	}
 
-    public void SaveData(TypeConfiguration typeConfiguration, string key, Variant data)
-    {
-        if(!canSave)
-            return;
+	public void SaveData(TypeConfiguration typeConfiguration, string key, Variant data)
+	{
+		if(!canSave)
+			return;
 
-        ConfigFile config = GetConfigFile();
-        config.SetValue(typeConfiguration.ToString(), key, data);
-        config.Save(GetFullPath());
-    }
+		ConfigFile config = GetConfigFile();
+		config.SetValue(typeConfiguration.ToString(), key, data);
+		config.Save(GetFullPath());
+	}
 
-    public Variant LoadData(TypeConfiguration typeConfiguration, string key, Variant defaultData = new Variant())
-    {
-        ConfigFile config = GetConfigFile();
-        return config.GetValue(typeConfiguration.ToString(), key, defaultData);
-    }
+	public Variant LoadData(TypeConfiguration typeConfiguration, string key, Variant defaultData = new Variant())
+	{
+		ConfigFile config = GetConfigFile();
+		return config.GetValue(typeConfiguration.ToString(), key, defaultData);
+	}
 
-    public int LoadIntData(TypeConfiguration typeConfiguration, string key, int defaultData = 0)
-        => LoadData(typeConfiguration, key, defaultData).AsInt32();
+	public int LoadIntData(TypeConfiguration typeConfiguration, string key, int defaultData = 0)
+		=> LoadData(typeConfiguration, key, defaultData).AsInt32();
 
-    public string LoadStringData(TypeConfiguration typeConfiguration, string key, string defaultData = "")
-        => LoadData(typeConfiguration, key, defaultData).AsString();
+	public string LoadStringData(TypeConfiguration typeConfiguration, string key, string defaultData = "")
+		=> LoadData(typeConfiguration, key, defaultData).AsString();
 
-    public float LoadFloatData(TypeConfiguration typeConfiguration, string key, float defaultData = 0)
-        => LoadData(typeConfiguration, key, defaultData).As<float>();
+	public float LoadFloatData(TypeConfiguration typeConfiguration, string key, float defaultData = 0)
+		=> LoadData(typeConfiguration, key, defaultData).As<float>();
 
-    public bool LoadBoolData(TypeConfiguration typeConfiguration, string key, bool defaultData = false)
-        => LoadData(typeConfiguration, key, defaultData).AsBool();
+	public bool LoadBoolData(TypeConfiguration typeConfiguration, string key, bool defaultData = false)
+		=> LoadData(typeConfiguration, key, defaultData).AsBool();
 
-    public Array LoadArrayData(TypeConfiguration typeConfiguration, string key, Array defaultData = default)
-        => LoadData(typeConfiguration, key, defaultData).AsGodotArray();
+	public Array LoadArrayData(TypeConfiguration typeConfiguration, string key, Array defaultData = default)
+		=> LoadData(typeConfiguration, key, defaultData).AsGodotArray();
 
-    public Dictionary LoadDictionaryData(TypeConfiguration typeConfiguration,
-                                         string key,
-                                         Dictionary defaultData = default)
-        => LoadData(typeConfiguration, key, defaultData).AsGodotDictionary();
+	public Dictionary LoadDictionaryData(TypeConfiguration typeConfiguration,
+										 string key,
+										 Dictionary defaultData = default)
+		=> LoadData(typeConfiguration, key, defaultData).AsGodotDictionary();
 
-    private ConfigFile GetConfigFile()
-    {
-        var result = new ConfigFile();
+	private ConfigFile GetConfigFile()
+	{
+		var result = new ConfigFile();
 
-        Error error = result.Load(GetFullPath());
+		Error error = result.Load(GetFullPath());
 
-        if(error == Error.FileNotFound){
-            error = result.Save(GetFullPath());
-        }
+		if(error == Error.FileNotFound){
+			error = result.Save(GetFullPath());
+		}
 
-        if(error != Error.Ok){
-            GD.PrintErr(error);
-        }
+		if(error != Error.Ok){
+			GD.PrintErr(error);
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    private string GetFullPath()
-    {
-        return Path.Combine(GetFolderPath(), fileName);
-    }
+	private string GetFullPath()
+	{
+		return Path.Combine(GetFolderPath(), fileName);
+	}
 
-    private string GetFolderPath()
-    {
-        return ProjectSettings.GlobalizePath(pathConfig);
-    }
+	private string GetFolderPath()
+	{
+		return ProjectSettings.GlobalizePath(pathConfig);
+	}
 }
 
 public enum TypeConfiguration
 {
-    Player,
-    Level,
-    Setting
+	Player,
+	Level,
+	Setting
 }
