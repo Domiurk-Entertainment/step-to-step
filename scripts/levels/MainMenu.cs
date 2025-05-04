a3142af2d884521e3130cd73205cbfcb2cb41fbe
Roma5640 <138190068+XxktotosxX@users.noreply.github.com>
2025-05-04 19:23:54 +0300

    new main menu + anim

--- scripts/levels/MainMenu.cs
+++ scripts/levels/MainMenu.cs
@@ -6,48 +6,48 @@ namespace StepToStep.Level;
 
 public partial class MainMenu : Node
 {
-    [Export(PropertyHint.File)] private string _sceneToPlay;
-    [Export(PropertyHint.File)] private Button continueButton;
+	[Export(PropertyHint.File)] private string _sceneToPlay;
+	[Export(PropertyHint.File)] private Button continueButton;
 
-    public override void _Ready()
-    {
-        // CheckExistData();
-    }
+	public override void _Ready()
+	{
+		// CheckExistData();
+	}
 
-    private void CheckExistData()
-    {
-        continueButton.Disabled = !Utils.Systems.Instance.SaveSystem.ExistSaves();
-    }
+	private void CheckExistData()
+	{
+		continueButton.Disabled = !Utils.Systems.Instance.SaveSystem.ExistSaves();
+	}
 
-    public void Start()
-    {
-        Utils.Systems.Instance.UserInterfaceSystem.Modal.Open("Start New Game",
-                                                                       "Are you sure you want to start the new game?",
-                                                                       "Yes", "No",
-                                                                       StartGame);
-        return;
+	public void Start()
+	{
+		Utils.Systems.Instance.UserInterfaceSystem.Modal.Open("Start New Game",
+																	   "Are you sure you want to start the new game?",
+																	   "Yes", "No",
+																	   StartGame);
+		return;
 
-        void StartGame()
-        {
-            Utils.Systems.Instance.SaveSystem.RemoveAllData();
-            Continue();
-        }
-    }
+		void StartGame()
+		{
+			Utils.Systems.Instance.SaveSystem.RemoveAllData();
+			Continue();
+		}
+	}
 
-    public void Exit()
-    {
-        GetTree().Quit();
-    }
+	public void Exit()
+	{
+		GetTree().Quit();
+	}
 
-    public void Continue()
-    {
-        PackedScene scene = GD.Load<PackedScene>(_sceneToPlay);
-        Utils.Systems.Instance.SceneTransition.ChangeScene(scene);
-    }
+	public void Continue()
+	{
+		PackedScene scene = GD.Load<PackedScene>(_sceneToPlay);
+		Utils.Systems.Instance.SceneTransition.ChangeScene(scene);
+	}
 
-    public void RemoveSavedData()
-    {
-        Utils.Systems.Instance.SaveSystem.RemoveAllData();
-        CheckExistData();
-    }
+	public void RemoveSavedData()
+	{
+		Utils.Systems.Instance.SaveSystem.RemoveAllData();
+		CheckExistData();
+	}
 }
--- scripts/utilities/Currency.cs
+++ scripts/utilities/Currency.cs
@@ -6,49 +6,49 @@ namespace StepToStep.Utils;
 
 public partial class Currency : HSplitContainer
 {
-    public static Currency Instance;
+	public static Currency Instance;
 
-    [Export] private ItemResource _resource;
-    [field: Export] public int Coins { get; private set; } = 0;
+	[Export] private ItemResource _resource;
+	[field: Export] public int Coins { get; private set; } = 0;
 
-    [Export] private SectionType _sectionType = SectionType.Player;
+	[Export] private SectionType _sectionType = SectionType.Player;
 
-    private TextureRect _textureRect;
-    private Label _label;
+	private TextureRect _textureRect;
+	private Label _label;
 
-    public override void _Ready()
-    {
-        if(Instance != null && Instance != this){
-            GD.Print("It duplicate.");
-            QueueFree();
-        }
-        else{
-            Instance = this;
-        }
+	public override void _Ready()
+	{
+		if(Instance != null && Instance != this){
+			GD.Print("It duplicate.");
+			QueueFree();
+		}
+		else{
+			Instance = this;
+		}
 
-        _label = this.FindNode<Label>();
-        _textureRect = this.FindNode<TextureRect>();
-    }
+		_label = this.FindNode<Label>();
+		_textureRect = this.FindNode<TextureRect>();
+	}
 
-    public void Update()
-    {
-        _label.Text = Coins.ToString();
-        _textureRect.Texture = _resource.Icon;
-    }
+	public void Update()
+	{
+		_label.Text = Coins.ToString();
+		_textureRect.Texture = _resource.Icon;
+	}
 
-    public override void _EnterTree()
-    {
-        Hide();
-        // Coins = SaveSystem.Instance.Get(_sectionType, GetKey("Coins"), 0).AsInt32();
-    }
+	public override void _EnterTree()
+	{
+		Hide();
+		// Coins = SaveSystem.Instance.Get(_sectionType, GetKey("Coins"), 0).AsInt32();
+	}
 
-    public override void _ExitTree()
-    {
-        Save();
-    }
+	public override void _ExitTree()
+	{
+		Save();
+	}
 
-    public void Save()
-    {
-        SaveSystem.Instance.Set(_sectionType, this.GetSaveKey(), Coins);
-    }
+	public void Save()
+	{
+		SaveSystem.Instance.Set(_sectionType, this.GetSaveKey(), Coins);
+	}
 }
