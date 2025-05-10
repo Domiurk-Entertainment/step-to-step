0378a19d8299de2bf7a2288efe6dc0aae3be75fa
Roma5640 <138190068+XxktotosxX@users.noreply.github.com>
2025-05-10 13:45:54 +0300

    chahge zoom cam

--- scripts/systems/SoundSystem.cs
+++ scripts/systems/SoundSystem.cs
@@ -7,26 +7,26 @@ namespace StepToStep.Systems;
 
 public partial class SoundSystem : System<SoundSystem>
 {
-    private List<AudioStreamPlayer> _sounds;
+	private List<AudioStreamPlayer> _sounds;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        _sounds = new List<AudioStreamPlayer>(GetChildren().Cast<AudioStreamPlayer>());
-    }
+	public override void _Ready()
+	{
+		base._Ready();
+		_sounds = new List<AudioStreamPlayer>(GetChildren().Cast<AudioStreamPlayer>());
+	}
 
-    public void Add(AudioStreamPlayer sound)
-    {
-        var duplicate = sound.Duplicate() as AudioStreamPlayer;
-        _sounds.Add(duplicate);
-        AddChild(duplicate);
-        sound.QueueFree();
-    }
+	public void Add(AudioStreamPlayer sound)
+	{
+		var duplicate = sound.Duplicate() as AudioStreamPlayer;
+		_sounds.Add(duplicate);
+		AddChild(duplicate);
+		sound.QueueFree();
+	}
 
-    public void TryPlay(string name)
-    {
-        AudioStreamPlayer sound =
-            _sounds.FirstOrDefault(a => string.Equals(a.Name, name, StringComparison.CurrentCultureIgnoreCase));
-        sound?.Play();
-    }
+	public void TryPlay(string name)
+	{
+		AudioStreamPlayer sound =
+			_sounds.FirstOrDefault(a => string.Equals(a.Name, name, StringComparison.CurrentCultureIgnoreCase));
+		sound?.Play();
+	}
 }
